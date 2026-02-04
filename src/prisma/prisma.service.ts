f7e13f9ee05147f5c6886242fa40f8ff17bd1f44
johnpaulsale <iamjohnpaulsale12@gmail.com>
2026-02-04 21:35:40 +0800

    fix: change db provider into mysql

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { PrismaMssql } from '@prisma/adapter-mssql';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 
 @Injectable()
 export class PrismaService
@@ -9,7 +9,7 @@ export class PrismaService
 {
   constructor() {
     const connectionString = process.env.DATABASE_URL || '';
-    const adapter = new PrismaMssql(connectionString);
+    const adapter = new PrismaMariaDb(connectionString);
 
     super({ adapter });
   }
