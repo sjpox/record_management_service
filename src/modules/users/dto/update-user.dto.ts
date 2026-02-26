2058ef090f52fd3c0a63e42f5894e56343515208
johnpaulsale <iamjohnpaulsale12@gmail.com>
2026-02-26 23:06:00 +0800

    feat: add mobile validation

--- src/modules/users/dto/update-user.dto.ts
+++ src/modules/users/dto/update-user.dto.ts
@@ -1,12 +1,16 @@
 import { PartialType, OmitType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
-import { IsOptional, IsString, MinLength } from 'class-validator';
+import { IsOptional, IsString, MinLength, Matches } from 'class-validator';
 import { ApiPropertyOptional } from '@nestjs/swagger';
 
 export class UpdateUserDto extends PartialType(OmitType(CreateUserDto, ['Password'] as const)) {
-  @ApiPropertyOptional({ minLength: 6 })
+  @ApiPropertyOptional({ minLength: 8 })
   @IsOptional()
   @IsString()
-  @MinLength(6)
+  @MinLength(8, { message: 'Password must be at least 8 characters' })
+  @Matches(/[A-Z]/, { message: 'Password must contain an uppercase letter' })
+  @Matches(/[a-z]/, { message: 'Password must contain a lowercase letter' })
+  @Matches(/\d/, { message: 'Password must contain a number' })
+  @Matches(/[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/, { message: 'Password must contain a special character' })
   Password?: string;
 }
