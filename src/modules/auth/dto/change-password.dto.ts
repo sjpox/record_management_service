baf990119b935aab3b721e8faa6d20d9d67dbc44
johnpaulsale <iamjohnpaulsale12@gmail.com>
2026-07-20 13:04:51 +0800

    feat: apply password criteria for change password

--- src/modules/auth/dto/change-password.dto.ts
+++ src/modules/auth/dto/change-password.dto.ts
@@ -1,10 +1,14 @@
-import { IsString, MinLength } from 'class-validator';
+import { IsString, MinLength, Matches } from 'class-validator';
 
 export class ChangePasswordDto {
   @IsString()
   currentPassword: string;
 
   @IsString()
-  @MinLength(6)
+  @MinLength(8, { message: 'Password must be at least 8 characters' })
+  @Matches(/[A-Z]/, { message: 'Password must contain an uppercase letter' })
+  @Matches(/[a-z]/, { message: 'Password must contain a lowercase letter' })
+  @Matches(/\d/, { message: 'Password must contain a number' })
+  @Matches(/[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/, { message: 'Password must contain a special character' })
   newPassword: string;
 }
